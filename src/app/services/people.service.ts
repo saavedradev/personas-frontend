e30a656560fcb504bec2f958c3fe783b504d55f5
saavedradev <saavedra.dev@gmail.com>
2021-05-30 11:50:09 -0500

    changes for person model

--- src/app/services/people.service.ts
+++ src/app/services/people.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+import { Person } from '../models/person';
 @Injectable({
   providedIn: 'root'
 })
 export class PeopleService {
 
-  constructor() { }
+  private urlEndPoint: string = 'http://localhost:8080/api/people';
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getPeople(): Observable<any> {
+    return this.http.get(this.urlEndPoint).pipe(
+      map((response: any) => {
+        (response.content as Person[]).map(person => {
+          person.fullName = person.fullName.toUpperCase();
+          return person;
+        });
+        return response;
+      })
+    );
+  }
 }
